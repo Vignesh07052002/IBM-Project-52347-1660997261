5dbc9401d95d3fd0befe77ab1c95c1e7242b9de6
Vignesh07052002 <113876510+Vignesh07052002@users.noreply.github.com>
2022-10-18 20:21:32 +0530

    Create Assignment 2.py

---
+++ Assignment/Team member 2/Assignment 2.py
@@ -0,0 +1,14 @@
+import os
+import random
+for i in range (30,80):
+
+	h= random.randint(65,75)
+	t= random.randint(35,40)
+	temp = t
+	humidity = h 
+	print (t)
+	print (h)
+if (humidity >=75 or temp >=40):
+		print ("! ! HIGH ALERT ! !")
+	else:
+		print("LOW") 
